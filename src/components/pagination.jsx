489f826f5cf4b24e0ae26ab0c6e916521067b804
adamdgit <adam_d@outlook.com>
2023-02-15 01:15:20 +1100

    Added functionality for prev & next button

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -10,21 +10,33 @@ export default function Pagination({ products, setPage, itemsPerPage }) {
     setSelectedPage(Number(e))
   }
 
+  function previousPage() {
+    if (selectedPage === 1) return
+    setPage(selectedPage -2) // must be converted to index so -1 then also -1 for going back a page
+    setSelectedPage(selectedPage -1)
+  }
+
+  function nextPage() {
+    if (selectedPage === Math.ceil(products.length / itemsPerPage)) return
+    setPage(selectedPage) // don't increase because this is an index (start at 0)
+    setSelectedPage(selectedPage +1)
+  }
+
   return (
     <div className="pagination">
-      <button className="pag-btn">◀ Prev</button>
+      <button className="pag-btn" onClick={() => previousPage()}>◀ Prev</button>
       {
-        [...Array(Math.ceil(products.length / itemsPerPage))].map((a, index) => {
-          return <button 
+        [...Array(Math.ceil(products.length / itemsPerPage))].map((a, index) => 
+          <button 
             style={selectedPage === index +1 ? {background: 'white', border: '1px solid #2b87e2'} : {}}
             key={index} 
             onClick={(e) => paginationHandler(e.target.innerText)}
             className="pag-btn">
               {index +1}
           </button>
-        })
+        )
       }
-      <button className="pag-btn">Next ▶</button>
+      <button className="pag-btn" onClick={() => nextPage()}>Next ▶</button>
     </div>
   )
 }
